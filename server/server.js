5163a35a279e8e83e73f5d12a051c6d97bb57020
Pratik <93414233+pratik-git10@users.noreply.github.com>
2026-06-03 23:18:57 +0530

    updated prisma

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import authRouter from "./routes/auth.route";
+import authRouter from "./src/routes/auth.route.js";
+import prisma from "./configs/prisma.js";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,6 +12,27 @@ app.use(cors());
 
 app.use("/api/auth/user", authRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Store Rating API Running",
+  });
 });
+
+// app.use("/api/admin", adminRouter);
+
+async function startServer() {
+  try {
+    await prisma.$connect();
+
+    console.log("Database Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+startServer();
