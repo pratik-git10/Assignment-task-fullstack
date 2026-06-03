2b31b19b9143f8880ed84a903911781c94ba93f6
Pratik <93414233+pratik-git10@users.noreply.github.com>
2026-06-03 23:58:41 +0530

    updated prisma configuration

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -1,6 +1,9 @@
-import pkg from "@prisma/client";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../lib/generated/prisma/index.js";
 
-const { PrismaClient } = pkg;
-const prisma = new PrismaClient();
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString }); // <-- Passing connectionString directly in object
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
